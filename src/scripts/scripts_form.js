6a34fa4efbe2004f390565e2066e656ee79b0176
Ольга Малышева <olga.maysheva.smm@gmail.com>
2022-11-19 16:08:53 +0200

    регистрация

--- src/scripts/scripts_form.js
+++ src/scripts/scripts_form.js
@@ -1,12 +1,20 @@
 
-function HelloMessage() {
-    let name = document.getElementById('user_name').value;
-
-    if (name) {
-        alert(`Привет, ${name}!`);
-      } 
-      else {
-        alert('Напишите пожалуйста ваше имя!');
-      }
+let errors = [];
+function checkValidity(input) {
+  let validity = input.validity;
+  if (validity.valueMissing) { errors.push('Поле' + input.placeholder +  'не заполнено');}
+  if (validity.paternMismatch) { errors.push('Пароль должен состоять из цифр и латинских букв верхнего и нижнего регистра и не более 6 символов');}
+  if (validity.rangeOverflow) {let max = getAttributeValue(input, 'max'); errors.push('Максимальное значение не может быть больше чем ' + max);}
+  if (validity.rangeUnderflow) { let min = getAttributeValue(input, 'min'); errors.push('Максимальное значение не может быть меньше чем ' + min);}
 }
 
+function checkAll() {
+  errors = [];
+  let inputs = document.querySelectorAll("input");
+
+  for (let input of inputs) {
+    checkValidity(input);
+  }
+
+  document.getElementById('errorsInfo').innerHTML= errors.join('.<br>');
+}
